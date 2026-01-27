1476f0c4c9b977e7079cc103861969937626be24
rafaelmarzoratti <rafaelmarzoratti02@gmail.com>
2026-01-27 04:01:59 -0300

    removendo arquivos padrões

--- src/OrderManagement.Customers/OrderManagement.Customers.API/obj/Debug/net8.0/OrderManagement.Customers.API.AssemblyInfo.cs
+++ src/OrderManagement.Customers/OrderManagement.Customers.API/obj/Debug/net8.0/OrderManagement.Customers.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderManagement.Customers.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0900297efff53f9ed8fe09c3dc261bb75bb4a00d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderManagement.Customers.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderManagement.Customers.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
